3b8b43497449bb19ace75212b526a8245c3c5112
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2023-10-25 22:55:31 +0530

    Update main.py
    Signed-off-by: Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ import requests
 waka_key = os.getenv("INPUT_WAKATIME_API_KEY")
 stats_range = os.getenv("INPUT_STATS_RANGE", "last_7_days")
 
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 def this_range(dates: list) -> str:
     """Returns streak within given range"""
     range_end = datetime.datetime.strptime(dates[4], "%Y-%m-%dT%H:%M:%SZ")
